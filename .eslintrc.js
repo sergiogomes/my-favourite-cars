c6dc31d524375da56c2cee9697a32682a6d10124
Sergio Gomes <sergiopgomes92@gmail.com>
2022-09-11 20:53:18 -0300

    build: :construction: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'react-app',
+    'react-app/jest',
+    'airbnb',
+    'airbnb-typescript',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': ['off'],
+    'react/jsx-uses-react': ['off'],
+    'react/jsx-props-no-spreading': ['warn'],
+    'react/no-unescaped-entities': ['off'],
+    indent: ['off'],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+};
