d460b50fbf01241f0d087e86bf00c2344a51674b
sergiogomes <sergiopgomes92@gmail.com>
2022-09-16 00:33:55 -0300

    style: :lipstick: add a couple more

--- src/style/blocks/StyleSideBar/Logo.tsx
+++ src/style/blocks/StyleSideBar/Logo.tsx
@@ -2,6 +2,11 @@ import styled from 'styled-components';
 
 const Logo = styled.div`
   padding: 80px 20px;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
   border-bottom: 1px solid #eaeaea;
 `;
 
--- src/style/blocks/StyleSideBar/StyleSideBar.tsx
+++ src/style/blocks/StyleSideBar/StyleSideBar.tsx
@@ -17,6 +17,7 @@ const StyledSideBar: any = styled.aside<AsideProps>`
   flex-direction: column;
   align-items: center;
   justify-content: space-around;
+  border-right: 1px solid #eaeaea;
   transition: 2s;
 
   ${({ visibility }) =>
